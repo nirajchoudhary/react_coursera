6ee50a26760a3d7236eacb34bc857637451db123
niraj_choudhary <nirajchoudhary03@gmail.com>
2018-10-21 00:03:56 +0530

    react coursera exercise -1

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText } from 'reactstrap';
+
+
+class Menu extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish !== null) {
+            return (
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <Card>
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-8">
+                        {
+                            dish.comments.map((comment) => {
+                                return (
+                                    <blockquote key={comment.id} className="blockquote text-left">
+                                        <p className="mb-0">{comment.comment}</p>
+                                        <footer className="blockquote-footer text-right">{comment.author} <cite >{comment.date}</cite></footer>
+                                    </blockquote>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            )
+
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+
+    render() {
+
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 col-md-4 mb-2">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            )
+        });
+
+        return(
+            <div className="container">
+                <div className="row mt-2">
+                    { menu }
+                </div>
+                <hr />
+                <div className="row mt-1">
+                    <div className="col-12 m-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Menu;
---
+++ src/shared/dishes.js
@@ -0,0 +1,107 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comments: [
+                {
+                    id: 0,
+                    rating: 4,
+                    comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                    author: "asjgdas asgdyas",
+                    date: "2012-03-05T17:57:28.6765Z"
+                },
+                {
+                    id: 1,
+                    rating: 5,
+                    comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                    author: "asjgdas asgdyas",
+                    date: "2014-03-05T17:57:28.6765Z"
+                }
+            ]
+        },
+        {
+        id: 1,
+        name:'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        comments: [
+            {
+                id: 0,
+                rating: 4,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "asjgdas asgdyas",
+                date: "2012-03-05T17:57:28.6765Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "asjgdas asgdyas",
+                date: "2014-03-05T17:57:28.6765Z"
+            }
+        ]
+        },
+        {
+        id: 2,
+        name:'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        comments: [
+            {
+                id: 0,
+                rating: 4,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "asjgdas asgdyas",
+                date: "2012-03-05T17:57:28.6765Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "asjgdas asgdyas",
+                date: "2014-03-05T17:57:28.6765Z"
+            }
+        ]
+        },
+        {
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 4,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "sdfs sdgdsg fgjfgjf",
+                date: "2012-03-05T17:57:28.6765Z"
+            },
+            {
+                id: 1,
+                rating: 5,
+                comment: "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+                author: "sgdhgf jghkuhk fhdg",
+                date: "2014-03-05T17:57:28.6765Z"
+            }
+        ]
+        }
+    ];
